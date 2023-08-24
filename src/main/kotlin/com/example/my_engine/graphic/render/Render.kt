7974ec163de61af2d63040c18e7df9f34e93ca4b
Maor Barak <maor@vhive.ai>
2023-08-24 19:26:13 +0300

    back culling

--- src/main/kotlin/com/example/my_engine/graphic/render/Render.kt
+++ src/main/kotlin/com/example/my_engine/graphic/render/Render.kt
@@ -10,7 +10,12 @@ class Render(val window: Window) {
 
     fun render(scene: Scene) {
 
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_DEPTH_TEST) // requires to know which vertex to draw above other
+
+        // In OpenGL, by default, triangles that are in counter-clockwise order are facing towards the viewer and triangles that are in clockwise order are facing backwards.
+        GL11.glEnable(GL11.GL_CULL_FACE) // disable faces that are in X direction.
+        GL11.glCullFace(GL11.GL_BACK) // indicates which is the X direction
+
         // glViewport(0, 0, window.getWidth(), window.getHeight());
         sceneRenderer.render(scene)
     }
